e788725b602184a7085797915d64e678835c2b4f
Calvin James Maximo <calvinjames.maximo@gcash.com>
2025-08-12 12:15:12 +0800

    feat: finish Exercise 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,21 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        /* Exercise 1
+        * Predicted output:
+        * Tuut, tuut!
+        * I am a Generic Vehicle Mustang
+        * */
+        Car myCar = new Car();
+        myCar.honk();
+        myCar.display();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Exercise 1 Mini Challenge
+//        Bicycle myBicycle = new Bicycle();
+//        System.out.print("Bicycle says: ");
+//        myBicycle.honk();
+//        System.out.print("Bicycle says: ");
+//        myBicycle.ringBell();
     }
 }
