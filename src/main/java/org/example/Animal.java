cefd2cce4bb335b12f935b29181daadbf03bc6d0
Calvin James Maximo <calvinjames.maximo@gcash.com>
2025-08-12 12:29:42 +0800

    feat: finish Exercise 2

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public abstract class Animal {
+    public abstract void makeSound();
+
+    public String breathe() {
+        return "Inhale... Exhale...";
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,9 @@ public class Main {
         * Tuut, tuut!
         * I am a Generic Vehicle Mustang
         * */
-        Car myCar = new Car();
-        myCar.honk();
-        myCar.display();
+//        Car myCar = new Car();
+//        myCar.honk();
+//        myCar.display();
 
         // Exercise 1 Mini Challenge
 //        Bicycle myBicycle = new Bicycle();
@@ -17,5 +17,20 @@ public class Main {
 //        myBicycle.honk();
 //        System.out.print("Bicycle says: ");
 //        myBicycle.ringBell();
+
+        /* Exercise 2
+        * Predicted output:
+        * The area is: 78.5...
+        * The area is: 24.0
+        * */
+//        Circle myCircle = new Circle();
+//        System.out.println(myCircle.display());
+//
+//        Rectangle myRectangle = new Rectangle();
+//        System.out.println(myRectangle.display());
+
+        Dog myDog = new Dog();
+        System.out.println(myDog.breathe());
+        myDog.makeSound();
     }
 }
